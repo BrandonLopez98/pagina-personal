c048421da2b8872d61c016a63a29f5be1bfd8d43
Esteban Lopez <brandonlopezmonroy@gmail.com>
2023-09-06 15:27:13 -0500

    actualizado a hoy

--- main.js
+++ main.js
@@ -53,3 +53,16 @@ btnNext.addEventListener('click', nextItem);
 // Actualizar el carrusel al cargar la página
 updateCarousel();
 startAutoMove();
+
+
+function showImage(src) {
+  var modal = document.getElementById('modal');
+  var modalImage = document.getElementById('modal-image');
+  modal.style.display = 'block';
+  modalImage.src = src;
+}
+
+function hideImage() {
+  var modal = document.getElementById('modal');
+  modal.style.display = 'none';
+}
